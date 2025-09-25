c72ab37431f4686e736e94f4f35b2ec2bcfcd25d
Himanshu Mishra <mishrah085@gmail.com>
2025-09-25 13:44:28 +0530

    Time: 1551 ms (58.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,8 @@
+select 'High Salary' as category, count(*) as accounts_count
+from accounts where income > 50000
+union all
+select 'Average Salary' as category, count(*) as accounts_count
+from accounts where income between 20000 and 50000
+union all
+select 'Low Salary' as category, count(*) as accounts_count
+from accounts where income < 20000
