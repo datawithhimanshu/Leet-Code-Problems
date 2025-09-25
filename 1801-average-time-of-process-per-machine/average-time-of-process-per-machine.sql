dcc0007fd5a9de8d41e4da46796f48e83057a4a8
Himanshu Mishra <mishrah085@gmail.com>
2025-09-25 13:36:17 +0530

    Time: 693 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+with cte as (select machine_id, process_id, 
+max(case when activity_type = 'start' then timestamp end) as start_timestamp,
+max(case when activity_type = 'end' then timestamp end) as end_timestamp
+from activity
+group by machine_id, process_id)
+select machine_id , round(avg(end_timestamp - start_timestamp),3) as processing_time from cte
+group by machine_id;
