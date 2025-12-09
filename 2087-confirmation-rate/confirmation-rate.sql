3c5b9530b86d48f04266ea4927abf1251e122b4d
Himanshu Mishra <mishrah085@gmail.com>
2025-12-09 13:15:18 +0530

    Time: 650 ms (35.66%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,5 +1,6 @@
-select s.user_id,
-round(sum(case when c.action = 'confirmed' then 1 else 0 end)/count(*) ,2) as confirmation_rate
-from signups as s
-left join confirmations as c on s.user_id = c.user_id
-group by s.user_id;
+# Write your MySQL query statement below
+select a.user_id,
+round(count(case when b.action = 'confirmed' then 1 else null end)/count(*),2) as confirmation_rate
+from signups as a
+left join confirmations as b on a.user_id = b.user_id
+group by a.user_id;
