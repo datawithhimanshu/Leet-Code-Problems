d68e7e734551594122982c35032d84ea92f92765
Himanshu Mishra <mishrah085@gmail.com>
2025-12-09 12:29:04 +0530

    Time: 236 ms (54.86%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,9 @@
-with cte as (select machine_id, process_id, 
-max(case when activity_type = 'start' then timestamp end) as start_timestamp,
-max(case when activity_type = 'end' then timestamp end) as end_timestamp
+# Write your MySQL query statement below
+with cte as(select machine_id, process_id,
+max(case when activity_type = "start" then timestamp end) as start_timestamp,
+max(case when activity_type = "end" then timestamp end) as end_timestamp
 from activity
 group by machine_id, process_id)
-select machine_id , round(avg(end_timestamp - start_timestamp),3) as processing_time from cte
+select machine_id, round(avg(end_timestamp - start_timestamp),3) as processing_time
+from cte
 group by machine_id;
