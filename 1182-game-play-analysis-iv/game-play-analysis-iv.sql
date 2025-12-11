2a65fa9d3df42fde74eaeafd56aef7d2a3299fee
Himanshu Mishra <mishrah085@gmail.com>
2025-12-11 10:08:51 +0530

    Time: 510 ms (79.23%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,6 +1,6 @@
-with firstlogin as(select player_id, min(event_date) as first_login
+with cte as(select player_id, min(event_date) as first_login
 from activity
 group by player_id)
 select round(count(a.player_id)/(select count(distinct player_id) from activity),2) as fraction
 from activity as a
-join firstlogin as f on datediff(a.event_date,f.first_login) = 1 and a.player_id = f.player_id;
+join cte as c on datediff(a.event_date,c.first_login) = 1 and a.player_id = c.player_id;
