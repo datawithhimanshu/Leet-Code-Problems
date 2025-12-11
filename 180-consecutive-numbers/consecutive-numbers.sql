e591c3467ad149d67ba63b0637dc2d1ef76ea9f0
Himanshu Mishra <mishrah085@gmail.com>
2025-12-11 11:55:16 +0530

    Time: 600 ms (16.50%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,8 +1,8 @@
-with cte as (select id, num,
-row_number() over (partition by num order by id) as rn,
-id - row_number() over (partition by num order by id) as diff
+# Write your MySQL query statement below
+with cte as(select *, 
+row_number() over(partition by num order by id) as rn,
+id - row_number() over(partition by num order by id) as diff
 from logs)
 select distinct num as ConsecutiveNums from cte
 group by num,diff
-having count(*) >= 3;
-
+having count(*)>=3;
