0d7230badaa36a504adf41c8a5dc7432e0d1e462
Himanshu Mishra <mishrah085@gmail.com>
2025-12-11 12:18:17 +0530

    Time: 490 ms (42.06%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,7 +1,9 @@
-with cte as(select product_id,new_price,
-rank() over (partition by product_id order by change_date desc) as rnk
+# Write your MySQL query statement below
+with cte as(select *,
+row_number() over (partition by product_id order by change_date desc) as rn
 from products
-where change_date <= '2019-08-16'),
-all_products as (select distinct product_id from products)
-select a.product_id, coalesce(c.new_price,10) as price from all_products as a
-left join cte as c on a.product_id = c.product_id and c.rnk = 1;
+where change_date<= '2019-08-16'),
+all_product as (select distinct product_id from products)
+select a.product_id, coalesce(c.new_price,10) as price
+from all_product as a
+left join cte as c on a.product_id = c.product_id and c.rn = 1;
